3e59def12511f6106631719798ef4fde668cb37b
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2022-09-02 13:49:46 +0530

    Project 1

---
+++ FoodOrderingDesktopApp/src/main/java/Interfaces/Values.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interfaces;
+
+/**
+ *
+ * @author acer
+ */
+public interface Values {
+    public static final int ADMIN_ROLE=1;
+    public static final int MANAGER_ROLE=2;
+    
+    
+    public static final int USERNAME_ALREAD_IN_USE= -1;
+    
+    public static final int USER_ADDED=1;
+    public static final int USER_NOT_ADDED=0;
+    
+    
+    public static final int UPDATE_SUCCESSFUL=1;
+    public static final int UPDATE_UNSUCCESSFUL=0;
+    
+    public static final int NUMBER_ALREAD_IN_USE= -1;
+    
+    public static final int VEHICLE_ADDED=1;
+    public static final int VEHICLE_NOT_ADDED=0;
+    
+    public static final int NOT_RENTED=0;
+    public static final int IS_RENTED=1;
+}
